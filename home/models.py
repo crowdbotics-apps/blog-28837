c878f72d245ed3511127dc8cb317e5f778cd3afb
Crowdbotics <team@crowdbotics.com>
2021-07-15 17:45:37 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -9,6 +9,28 @@ class HomePage(models.Model):
 
 class CustomText(models.Model):
     "Generated Model"
-    title = models.CharField(
-        max_length=150,
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+    name = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True,
+    )
+    price = models.DecimalField(
+        max_digits=30,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        null=True,
+        blank=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        null=True,
+        blank=True,
     )
